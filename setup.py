369d40e7b98ac46e063015a8e0fd4a635a1782f6
Michał Sałaban <michal@salaban.info>
2018-11-11 01:52:48 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='ingwebservice',
+    version='0.1',
+    packages=['ingwebservice'],
+    url='',
+    license='MIT',
+    author='emesik',
+    author_email='michal@salaban.info',
+    description='ING WebService wrapper',
+    install_requires=['zeep'],
+)
